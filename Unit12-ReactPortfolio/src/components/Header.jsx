860c996848aa34abd31b55e8e0b7b1c9ed9b026c
Jose Cabrera <josemcabrerat@gmail.com>
2025-01-25 06:53:46 -0700

    I started to work in the project.

---
+++ Unit12-ReactPortfolio/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+function Header() {
+
+    return (
+        <h1>Jose Cabrera</h1>
+    );
+
+}//function Header
+
+export default Header;
---
+++ Unit12-ReactPortfolio/src/components/Project.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Header from './Header';
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+function Project() {
+
+return (
+    <div>
+       <Header />
+       <Navigation />
+       <Footer />
+    </div>
+);
+
+}//function Project
+
+export default Project;
