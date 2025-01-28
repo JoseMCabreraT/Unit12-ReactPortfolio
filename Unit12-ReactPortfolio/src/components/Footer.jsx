d1a537a43b52366bbfb8d1fb370da8737324f96d
Jose Cabrera <josemcabrerat@gmail.com>
2025-01-27 21:13:48 -0700

    I added more code on Footer.jsx.

--- Unit12-ReactPortfolio/src/components/Footer.jsx
+++ Unit12-ReactPortfolio/src/components/Footer.jsx
@@ -1,5 +1,27 @@
 function Footer() {
 
+    return (
+
+     <footer>
+        <ul>
+            <li>
+            <a href="https://github.com/JoseMCabreraT?ocid=AIDcmmcwpj1e5v_SEM__k_Cj0KCQjwpZWzBhC0ARIsACvjWRN4AWsloPKtmB8rEtyYCGo0OL3-_2ujUKvqQtt_gVA0m0lIiCdGu70aAq7_EALw_wcB_k_"></a>
+            </li>
+            <li>
+            <a href="https://www.linkedin.com/in/jose-cabrera-b8916492/"></a>
+            </li>
+            <li>
+            <a href="https://stackoverflow.com/users/3474054/josemcabrera"></a>
+            </li>
+            <li>
+            <a href="https://x.com/JoseMCabreraT"></a>
+            </li>
+        </ul>
+
+     </footer>
+
+    );
+
 
 }//function Footer
 
