c32f5a8a04726c8e41537ef907733d65536106f2
Jose Cabrera <josemcabrerat@gmail.com>
2025-05-09 06:17:20 -0600

    I'm updating ny portfolio webiste with a new design.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,16 +2,16 @@ function Navigation() {
   return (
     <ul className="nav">
       <li className="nav-item">
-        <h2><a href="#about" aria-label="Go to About Me section">About Me</a></h2>
+        <a href="#about" aria-label="Go to About Me section">About Me</a>
       </li>
       <li className="nav-item">
-        <h2><a href="#portfolio" aria-label="Go to Portfolio section">Portfolio</a></h2>
+        <a href="#portfolio" aria-label="Go to Portfolio section">Portfolio</a>
       </li>
       <li className="nav-item">
-        <h2><a href="#contact" aria-label="Go to Contact section">Contact</a></h2>
+        <a href="#contact" aria-label="Go to Contact section">Contact</a>
       </li>
       <li className="nav-item">
-        <h2><a href="#resume" aria-label="Go to Resume section">Resume</a></h2>
+        <a href="#resume" aria-label="Go to Resume section">Resume</a>
       </li>
     </ul>
   );
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,10 @@ function About() {
     return (
       <section id="about">
         <h2>About Me</h2>
-        <p>I'm Jose Cabrera, a full-stack developer passionate about building modern web applications...</p>
+        <p>I'm Jose Cabrera, a Full Stack Developer with a strong foundation in JavaScript,TypeScript, and the MERN stack 
+          (MongoDB, Express, React,Node.js), gained through hands-on experience building real-world applications. I transitioned 
+          into tech via an intensive coding bootcamp where I focused on modern development practices, agile workflows, and deploying 
+          full-stack apps from the ground up.</p>
       </section>
     );
   }
